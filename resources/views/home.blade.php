15e8a8a5b20a760a23dd87fe38f4a31620b70128
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-07-02 12:40:29 +0200

    V-2.1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da config/data.js: {{ $home }}</h1>
-<p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
+    <h1>Dati da config/data.php: {{ $home }}</h1>
+    <p>This is my paragraph content.</p>
+    <button class="btn btn-primary">Cliccami</button>
 @endsection
